eedf7d405c0a0c72410e76114b8d8da5a664d50d
jdewinne <joris.dewinne@gmail.com>
2015-10-01 15:32:33 +0200

    Made blockroot a bit more configurable

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ XLD TaskManager Plugin
 ===================
 
 XL Deploy plugin for showing an overview which tasks are ongoing.
+
+# xlDeployBlockRoot - XLD 4.5.x vs 5.x#
+
+* For 4.5.x - Make sure you override the `xlDeployBlockRoot` to a value `0` in `src/main/resources/web/xld-taskmanager-plugin/js/taskApp.js`
+
+* For 5.x - Make sure you override the `xlDeployBlockRoot` to a value `0_1_1` in `src/main/resources/web/xld-taskmanager-plugin/js/taskApp.js`
--- src/main/resources/web/xld-taskmanager-plugin/js/services/tasksService.js
+++ src/main/resources/web/xld-taskmanager-plugin/js/services/tasksService.js
@@ -1,11 +1,11 @@
 angular.module('TaskApp')
-    .factory('TasksService', function(xJsonHttp, xlDeployServicesRoot) {
+    .factory('TasksService', function(xJsonHttp, xlDeployServicesRoot, xlDeployBlockRoot) {
         return {
             loadTaskList: function() {
                 return xJsonHttp.get(xlDeployServicesRoot + 'tasks/v2/current/all', null, false);
             },
             loadTask: function(taskId) {
-                return xJsonHttp.get(xlDeployServicesRoot + 'tasks/v2/' + taskId + '/block/0_1_1', null, false);
+                return xJsonHttp.get(xlDeployServicesRoot + 'tasks/v2/' + taskId + '/block/' + xlDeployBlockRoot, null, false);
             }
         };
     });
